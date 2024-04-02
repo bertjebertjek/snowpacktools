e69dde55807c42fab22f5c465d3ca2a6246dda3d
tennismichel <michael.binder94@gmail.com>
2024-04-02 23:40:38 +0000

    caaml processing for NVE

--- snowpacktools/caaml/caamlv6_processor.py
+++ snowpacktools/caaml/caamlv6_processor.py
@@ -7,6 +7,7 @@
 
 import numpy as np
 from lxml import etree as et
+import os
 
 caaml_ns = "{http://caaml.org/Schemas/SnowProfileIACS/v6.0.3}"
 gml_ns = "{http://www.opengis.net/gml}"
@@ -18,41 +19,41 @@ def get_prof_metadata(file):
     xroot = tree.getroot()
     
     """Altitude, location, aspect"""
-    child_locRef = xroot.find(caaml_ns + 'locRef')
-    child_validElevation = child_locRef.find(caaml_ns + 'validElevation')
+    child_locRef            = xroot.find(caaml_ns + 'locRef')
+    child_validElevation    = child_locRef.find(caaml_ns + 'validElevation')
     child_ElevationPosition = child_validElevation.find(caaml_ns + 'ElevationPosition')
-    child_position = child_ElevationPosition.find(caaml_ns + 'position')
-    alt = float(child_position.text)
+    child_position          = child_ElevationPosition.find(caaml_ns + 'position')
+    alt                     = float(child_position.text)
 
     child_pointLocation = child_locRef.find(caaml_ns + 'pointLocation')
-    child_Point = child_pointLocation.find(gml_ns + 'Point') # be careful with namespace (gml)
-    child_pos = child_Point.find(gml_ns + 'pos')
-    lon_lat = child_pos.text
+    child_Point         = child_pointLocation.find(gml_ns + 'Point') # be careful with namespace (gml)
+    child_pos           = child_Point.find(gml_ns + 'pos')
+    lon_lat             = child_pos.text
 
     lon_lat = lon_lat.split(' ')
-    lon= float(lon_lat[0])
-    lat= float(lon_lat[1])
+    lon     = float(lon_lat[0])
+    lat     = float(lon_lat[1])
 
-    child_validAspect = child_locRef.find(caaml_ns + 'validAspect')
+    child_validAspect    = child_locRef.find(caaml_ns + 'validAspect')
     child_AspectPosition = child_validAspect.find(caaml_ns + 'AspectPosition')
-    child_positionA = child_AspectPosition.find(caaml_ns + 'position')
-    aspect = child_positionA.text
+    child_positionA      = child_AspectPosition.find(caaml_ns + 'position')
+    aspect               = child_positionA.text
 
-    child_validSlopeAngle = child_locRef.find(caaml_ns + 'validSlopeAngle')
+    child_validSlopeAngle    = child_locRef.find(caaml_ns + 'validSlopeAngle')
     child_SlopeAnglePosition = child_validSlopeAngle.find(caaml_ns + 'SlopeAnglePosition')
-    child_position_angle = child_SlopeAnglePosition.find(caaml_ns + 'position')
-    slope_angle = int(child_position_angle.text)
+    child_position_angle     = child_SlopeAnglePosition.find(caaml_ns + 'position')
+    slope_angle              = int(child_position_angle.text)
 
     """Date"""
-    child_timeRef = xroot.find(caaml_ns + 'timeRef')
-    child_recordTime =  child_timeRef.find(caaml_ns + 'recordTime')
-    child_TimeInstant = child_recordTime.find(caaml_ns + 'TimeInstant')
+    child_timeRef      = xroot.find(caaml_ns + 'timeRef')
+    child_recordTime   =  child_timeRef.find(caaml_ns + 'recordTime')
+    child_TimeInstant  = child_recordTime.find(caaml_ns + 'TimeInstant')
     child_timePosition = child_TimeInstant.find(caaml_ns + 'timePosition')
-    date = child_timePosition.text # string
+    date               = child_timePosition.text # string
 
     """Name"""
     child_name = child_locRef.find(caaml_ns + 'name')
-    name = child_name.text
+    name       = child_name.text
 
     prof_meta = {'lon'        : lon,
                  'lat'        : lat,
@@ -73,23 +74,51 @@ def add_snp_metadata(tree: object):
     child_metaData                  = xroot.find(caaml_ns + "metaData")
     child_customData                = child_metaData.find(caaml_ns + "customData")
     if not child_customData:
-        child_customData = et.SubElement(child_metaData, caaml_ns + "customData")
-    child_snp                       = et.SubElement(child_customData,caaml_ns + 'snp')
-    child_CanopyHeight              = et.SubElement(child_snp,caaml_ns + 'CanopyHeight')
-    child_CanopyBasalArea           = et.SubElement(child_snp,caaml_ns + 'CanopyBasalArea')
-    child_CanopyLAI                 = et.SubElement(child_snp,caaml_ns + 'CanopyLAI')
-    child_CanopyDirectThroughfall   = et.SubElement(child_snp,caaml_ns + 'CanopyDirectThroughfall')
-    child_SoilAlb                   = et.SubElement(child_snp,caaml_ns + 'SoilAlb')
-    child_ErosionLevel              = et.SubElement(child_snp,caaml_ns + 'ErosionLevel')
-    child_CanopyHeight.set("uom","m")
-    child_CanopyBasalArea.set("uom","m^2")
+        child_customData            = et.SubElement(child_metaData, caaml_ns + "customData")
+
+    child_snp                       = child_customData.find(caaml_ns + "snp")
+    if not child_snp:
+        """Add SNP relevant metadata to CAAML file, if not existing"""
+        child_snp                       = et.SubElement(child_customData,caaml_ns + 'snp')
+        child_CanopyHeight              = et.SubElement(child_snp,caaml_ns + 'CanopyHeight')
+        child_CanopyBasalArea           = et.SubElement(child_snp,caaml_ns + 'CanopyBasalArea')
+        child_CanopyLAI                 = et.SubElement(child_snp,caaml_ns + 'CanopyLAI')
+        child_CanopyDirectThroughfall   = et.SubElement(child_snp,caaml_ns + 'CanopyDirectThroughfall')
+        child_SoilAlb                   = et.SubElement(child_snp,caaml_ns + 'SoilAlb')
+        child_ErosionLevel              = et.SubElement(child_snp,caaml_ns + 'ErosionLevel')
+        child_CanopyHeight.set("uom","m")
+        child_CanopyBasalArea.set("uom","m^2")
+        
+        child_CanopyHeight.text             = "0.0"
+        child_CanopyBasalArea.text          = "0.0"
+        child_CanopyLAI.text                = "0.0"
+        child_CanopyDirectThroughfall.text  = "1.0"
+        child_SoilAlb.text                  = "0.2"
+        child_ErosionLevel.text             = "0.0"
+
+    child_locRef                = xroot.find(caaml_ns + "locRef")
     
-    child_CanopyHeight.text             = "0.0"
-    child_CanopyBasalArea.text          = "0.0"
-    child_CanopyLAI.text                = "0.0"
-    child_CanopyDirectThroughfall.text  = "1.0"
-    child_SoilAlb.text                  = "0.2"
-    child_ErosionLevel.text             = "0.0"
+    """Add name to location reference if missing"""
+    child_name                  = child_locRef.find(caaml_ns + "name")
+    if not child_name.text:
+        child_name.text = "nameless"
+
+    """Add slope angle to location reference if missing"""
+    child_validSlopeAngle       = child_locRef.find(caaml_ns + "validSlopeAngle")
+    if not child_validSlopeAngle:
+        child_validSlopeAngle    = et.SubElement(child_locRef,caaml_ns + "validSlopeAngle")
+        child_SlopeAnglePosition = et.SubElement(child_validSlopeAngle,caaml_ns + "SlopeAnglePosition")
+        child_position_angle     = et.SubElement(child_SlopeAnglePosition,caaml_ns + "position")
+        child_position_angle.text = str(0)
+
+    """Add aspect to location reference if missing"""
+    child_validAspect    = child_locRef.find(caaml_ns + "validAspect")
+    if not child_validAspect:
+        child_validAspect    = et.SubElement(child_locRef,caaml_ns + "validAspect")
+        child_AspectPosition = et.SubElement(child_validAspect,caaml_ns + "AspectPosition")
+        child_positionA      = et.SubElement(child_AspectPosition,caaml_ns + "position")
+        child_positionA.text = "N"
+    
 
     return tree
 
@@ -107,8 +136,13 @@ def add_monti_density(file, new_file):
     Comments:
         - If density exists from observation or simulation it is removed
     '''
+    try:
+        tree = et.parse(file)
+    except:
+        print(f"[i]  Reading of observed snow profile {file.split('/')[-1]} failed. File will be removed.")
+        os.remove(file)
+        return
 
-    tree = et.parse(file)
     tree = add_snp_metadata(tree)
     xroot = tree.getroot()
     
@@ -125,12 +159,31 @@ def add_monti_density(file, new_file):
     child_methodOfMeas = et.SubElement(child_densityMetaData, caaml_ns + 'methodOfMeas')
     child_methodOfMeas.text = 'other'
     
-    """Add density layer for each steatigraphy layer"""
+    """Check temperature profile (needs to be ascending order for SNP)"""
+    child_tempProfile = child_SnowProfileMeasurements.find(caaml_ns + 'tempProfile')
+    if not child_tempProfile:
+        print(f"[i]  Temperature profile missing for observed snow profile {file.split('/')[-1]}. File will be removed.")
+        os.remove(file)
+        return
+
+    # depths    = []
+    # temp_vals = []
+    # for obs in child_tempProfile.iter(caaml_ns + 'Obs'):
+    #     depths.append(obs.find(caaml_ns + 'depth').text)
+    #     temp_vals.append(obs.find(caaml_ns + 'snowTemp').text)
+    # print(depths)
+    # print(temp_vals)
+
+
+    """Add density layer for each stratigraphy layer"""
     child_stratProfile = child_SnowProfileMeasurements.find(caaml_ns + 'stratProfile')
     for layer in child_stratProfile.iter(caaml_ns + 'Layer'):
+        
+
         child_densityLayer = et.SubElement(child_densityProfile, caaml_ns + 'Layer')
         child_depthTop  = et.SubElement(child_densityLayer,caaml_ns + 'depthTop')
         child_thickness = et.SubElement(child_densityLayer,caaml_ns + 'thickness')
+
         child_density   = et.SubElement(child_densityLayer,caaml_ns + 'density')
 
         child_depthTop.set('uom', 'cm')
@@ -144,25 +197,29 @@ def add_monti_density(file, new_file):
         
         """Get grain forms and hardness to estimate density"""
         for form in layer.iter(caaml_ns + 'grainFormPrimary'):
+            form.text = check_grainType(form.text)
             grainFormPrimary = form.text
         grainFormSecondary = ""
         for form in layer.iter(caaml_ns + 'grainFormSecondary'):
+            form.text = check_grainType(form.text)
             grainFormSecondary = form.text
         for val_hardness in layer.iter(caaml_ns + 'hardness'):
-            hardness = val_hardness.text
+            val_hardness.text = check_handHardness(val_hardness.text)
+            handhardness      = val_hardness.text
         
-        child_density.text = str(_monti_density(grainFormPrimary, grainFormSecondary, hardness))
+        child_density.text = str(_monti_density(grainFormPrimary, grainFormSecondary, handhardness))
 
         """Check LWC/wetness (only full values are allowed)"""
         for val_wetness in layer.iter(caaml_ns + 'wetness'):
-            if val_wetness.text == "D-M":
-                val_wetness.text = "M" # LWC: 0.01
-            elif val_wetness.text == "M-W":
-                val_wetness.text = "W" # LWC: 0.03
-            elif val_wetness.text == "W-V":
-                val_wetness.text = "V" # LWC: 0.08
-            elif val_wetness.text == "V-S":
-                val_wetness.text = "S" # LWC: 0.15
+            val_wetness.text = check_wetness(val_wetness.text)
+
+        """Check if grain size is available"""
+        if grainFormPrimary in ["PP","DF","FC","RG"]:
+            child_grainType = layer.find(caaml_ns + 'grainSize')
+            if not child_grainType:
+                print(f"[i]  Grain size is missing for observed snow profile {file.split('/')[-1]}. File will be removed.")
+                os.remove(file)
+                return
                 
     tree.write(new_file,encoding='UTF-8', method="xml", doctype='<?xml version="1.0" encoding="UTF-8"?>')
 
@@ -196,7 +253,7 @@ def _monti_density(form, form2, hardness_str):
     6   = I  = Ice
     """
     
-    dict_hardness = {'F-' : 1,
+    dict_hardness = {'F' : 1,
                      'F+' : 1.5,
                      '4F' : 2,
                      '4F+' : 2.5,
@@ -208,7 +265,7 @@ def _monti_density(form, form2, hardness_str):
                      'K+' : 5.5,
                      'I'  : 6}
     
-    dict_hardness_i =  {'F-' : 0,
+    dict_hardness_i =  {'F' : 0,
                         'F+' : 1,
                         '4F' : 2,
                         '4F+': 3,
@@ -255,6 +312,73 @@ def _monti_density(form, form2, hardness_str):
     density    = dict_shapes[form][hardness_i]
     
     return density
+
+
+def check_wetness(wet_input):
+    if wet_input == "D-M":
+        wet_output = "M" # LWC: 0.01
+    elif wet_input == "M-W":
+        wet_output = "W" # LWC: 0.03
+    elif wet_input == "W-V":
+        wet_output = "V" # LWC: 0.08
+    elif wet_input == "V-S":
+        wet_output = "S" # LWC: 0.15
+    else:
+        wet_output = wet_input
+    return wet_output
+
+
+def check_grainType(gt_input):
+    if gt_input == "PPdc":
+        gt_output = "PP"
+    elif gt_input == "DFdc":
+        gt_output = "DF"
+    elif gt_input == "MFcl":
+        gt_output = "MF"
+    elif gt_input == "MFpc":
+        gt_output = "MF"
+    elif gt_input == "MFsl":
+        gt_output = "MF"
+    elif gt_input == "RGsr":
+        gt_output = "RG"
+    elif gt_input == "RGlr":
+        gt_output = "RG"
+    elif gt_input == "RGwp":
+        gt_output = "RG"
+    elif gt_input == "RGxf":
+        gt_output = "RG"
+    elif gt_input == "DHxr":
+        gt_output = "DH"
+    elif gt_input == "SHxr":
+        gt_output = "SH"
+    else:
+        gt_output = gt_input
+    return gt_output
+
+
+def check_handHardness(hh_input):
+    if hh_input == "F-":
+        hh_output = "F"
+    elif hh_input == "F-4F":
+        hh_output = "F+"
+    elif hh_input == "4F-":
+        hh_output = "F+"
+    elif hh_input == "4F-1F":
+        hh_output = "4F+"
+    elif hh_input == "1F-":
+        hh_output = "4F+"
+    elif hh_input == "1F-P":
+        hh_output = "1F+"
+    elif hh_input == "P-":
+        hh_output = "1F+"
+    elif hh_input == "P-K":
+        hh_output = "P+"
+    elif hh_input == "K-I":
+        hh_output = "K+"
+    else:
+        hh_output = hh_input
+
+    return hh_output
 
 
 if __name__ == "__main__":
